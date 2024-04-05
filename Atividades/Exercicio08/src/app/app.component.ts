3a94d5ae441cc50d45310dc5a32016571d560cac
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-04-05 10:30:14 -0300

    Add files via upload

---
+++ Atividades/Exercicio08/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'exercicio_08';
+}
